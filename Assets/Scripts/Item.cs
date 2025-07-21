d26f3e2b72d2e4aba079828d3c4b27639d190f99
Sterling Stokes <sterlingjstokes@gmail.com>
2025-07-21 05:16:19 -0400

    Prototyping 7/21
    Added simple push mechanic

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using Interact;
+using Unity.VisualScripting;
+public class Item : MonoBehaviour, IPushable
+{
+    private void OnCollisionStay(Collision other) {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //Todo Calculate the vector the player is pushing from
+            Rigidbody rb = this.GetComponent<Rigidbody>();
+            if (rb == null) return;
+
+            //Todo Null Check
+            Vector3 pushVector = this.transform.position - other.collider.bounds.center;
+            pushVector.y = 0f;
+            //  Debug.Log(pushVector);
+            Debug.DrawRay(other.collider.bounds.center, pushVector.normalized * 2f, Color.green);
+            rb.constraints = RigidbodyConstraints.FreezeRotation;
+            rb.AddForce(pushVector.normalized * 2f, ForceMode.Impulse);
+        }
+    }
+    void FixedUpdate()
+    {
+        
+    }
+
+}
+
+namespace Interact
+{
+    interface IPushable
+    {
+
+    }
+}
